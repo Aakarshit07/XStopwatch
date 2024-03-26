c3418183ec048216a62d95f0ce144877d3f949a0
Aakarshit07 <aakarshit070@gmail.com>
2024-03-26 19:23:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timerId;
+    if(isRunning){
+      timerId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [isRunning, timer])
+
+  const formatTimer = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const remainingSeconds = secs % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`
+  }
+
+  const reset = () => {
+    setTimer(0)
+    setIsRunning(false);
+  } 
+    
+
+  return (
+    <div className='app'>
+        <h1>Stopwatch</h1>
+        <p>Timer: {formatTimer(timer)}</p>
+        <div className='btn-container'>
+        <button 
+        onClick={()=> setIsRunning(!isRunning)}
+        >{isRunning ? "Stop" : "Start"}</button>
+        <button 
+        onClick={reset}>Reset</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
